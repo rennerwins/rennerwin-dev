96153c434fe515dcf15c59789cd49028d32ee84e
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2021-03-20 16:00:00 +0700

    setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#667EEA',
+          50: '#FFFFFF',
+          100: '#FFFFFF',
+          200: '#EDEFFC',
+          300: '#C0CAF6',
+          400: '#93A4F0',
+          500: '#667EEA',
+          600: '#3958E4',
+          700: '#1C3DCE',
+          800: '#162FA1',
+          900: '#102274',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
